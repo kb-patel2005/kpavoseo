8fbfae71f2c276678b292fe776cb65f8dbc9ebee
kb-patel2005 <krishpatel1181@gmail.com>
2026-08-26 14:50:18 +0530

    site map fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -25,6 +25,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.8,
     },
+    {
+      url: `${baseUrl}/services`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
 
     {
       url: `${baseUrl}/terms-and-conditions`,
